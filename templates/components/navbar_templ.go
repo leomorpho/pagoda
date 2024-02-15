3e7b2381290fcb854301762756a4997b2f4c4320
Léo Audibert <leonard.audibert@gmail.com>
2024-02-15 10:06:49 -0800

    Improve dropdowns to close when clicked

--- templates/components/navbar_templ.go
+++ templates/components/navbar_templ.go
@@ -57,7 +57,7 @@ func Navbar(page *controller.Page) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!-- Multi-Level Dropdown --><div class=\"relative\" x-data=\"{ open: false }\"><button @click=\"open = !open\" class=\"ml-3 text-sm rounded-full text-white hover:bg-gray-700 px-3 py-2 flex items-center justify-between\">Services <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"w-5 h-5\"><path fill-rule=\"evenodd\" d=\"M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z\" clip-rule=\"evenodd\"></path></svg></button><div x-show=\"open\" @click.away=\"open = false\" class=\"absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20\"><a href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Web Development</a> <a href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">SEO Services</a> <a href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Digital Marketing</a></div></div><!-- Example Menu Items --><div class=\"hidden md:flex items-center space-x-4\"><a hx-get=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!-- Multi-Level Dropdown --><div class=\"relative\" x-data=\"{ open: false }\"><button @click=\"open = !open\" class=\"ml-3 text-sm rounded-full text-white hover:bg-gray-700 px-3 py-2 flex items-center justify-between\">Services <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"w-5 h-5\"><path fill-rule=\"evenodd\" d=\"M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z\" clip-rule=\"evenodd\"></path></svg></button><div x-show=\"open\" @click.away=\"open = false\" class=\"absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20\"><a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Web Development</a> <a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">SEO Services</a> <a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Digital Marketing</a></div></div><!-- Example Menu Items --><div class=\"hidden md:flex items-center space-x-4\"><a hx-get=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -130,7 +130,7 @@ func Navbar(page *controller.Page) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		} else {
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul class=\"py-2\"><li><a hx-get=\"")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul class=\"py-2\"><li><a @click=\"open = false\" hx-get=\"")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -138,7 +138,7 @@ func Navbar(page *controller.Page) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" class=\"block px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer\">Login</a></li><li><a hx-get=\"")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" class=\"block px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer\">Login</a></li><li><a @click=\"open = false\" hx-get=\"")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
