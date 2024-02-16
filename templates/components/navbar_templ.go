8463a6378dcb0f4519554acba86aad8eb06d8306
Léo Audibert <leonard.audibert@gmail.com>
2024-02-15 16:02:33 -0800

    Use x-cloak so alpinejs navbar components don't blip on load

--- templates/components/navbar_templ.go
+++ templates/components/navbar_templ.go
@@ -49,7 +49,7 @@ func Navbar(page *controller.Page) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span></a><!-- Search Bar --><!-- Multi-Level Dropdown --><div class=\"relative\" x-data=\"{ open: false }\"><button @click=\"open = !open\" class=\"ml-3 text-sm rounded-full text-white hover:bg-gray-700 px-3 py-2 flex items-center justify-between\">Services <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"w-5 h-5\"><path fill-rule=\"evenodd\" d=\"M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z\" clip-rule=\"evenodd\"></path></svg></button><div x-show=\"open\" @click.away=\"open = false\" class=\"absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20\"><a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Web Development</a> <a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">SEO Services</a> <a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Digital Marketing</a></div></div><!-- Example Menu Items --><div class=\"hidden md:flex items-center space-x-4\"><a hx-get=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span></a><!-- Search Bar --><!-- Multi-Level Dropdown --><div class=\"relative\" x-data=\"{ open: false }\" x-cloak><button @click=\"open = !open\" class=\"ml-3 text-sm rounded-full text-white hover:bg-gray-700 px-3 py-2 flex items-center justify-between\">Services <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"w-5 h-5\"><path fill-rule=\"evenodd\" d=\"M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z\" clip-rule=\"evenodd\"></path></svg></button><div x-show=\"open\" @click.away=\"open = false\" class=\"absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20\"><a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Web Development</a> <a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">SEO Services</a> <a @click=\"open = false\" href=\"#\" class=\"block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100\">Digital Marketing</a></div></div><!-- Example Menu Items --><div class=\"hidden md:flex items-center space-x-4\"><a hx-get=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -73,7 +73,7 @@ func Navbar(page *controller.Page) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" class=\"text-sm text-white hover:bg-gray-700 px-3 py-2 rounded-full\">Contact</a></div><div class=\"relative\" x-data=\"{ open: false }\"><button type=\"button\" @click=\"open = !open\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" class=\"text-sm text-white hover:bg-gray-700 px-3 py-2 rounded-full\">Contact</a></div><div class=\"relative\" x-data=\"{ open: false }\" x-cloak><button type=\"button\" @click=\"open = !open\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
